5f348f44ea6fbca5fcd115a75103f6a1049157f7
JoshCodedit <joshua.dixoncms@gmail.com>
2024-12-03 16:45:33 +0000

    chore: undo image deleetion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,6 +25,14 @@ export default function Home() {
   return (
     <Box w="100%" h="100vh" bg="white" safeArea>
       <VStack space={4} flex={1} justifyContent="center" alignItems="center">
+      {/* <Image
+          source={{
+            uri: '/pwa-512x512.png'
+          }}
+          alt="Inside My Closet"
+          size="2xl"
+          resizeMode="contain"
+        /> */}
         {renderComponent()}
       </VStack>
 
